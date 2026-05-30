8516d04be7a35dc4d83f7fd5ec88a2e8384ec208
Ravi Paliwal <ravipaliwal885078@gmail.com>
2026-05-30 18:43:10 +0530

    Structural Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { powerApps } from "@microsoft/power-apps-vite/plugin"
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), powerApps()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
